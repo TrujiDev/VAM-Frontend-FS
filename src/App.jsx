79ba5eb094e627a36fd262949a8e6028ec394d26
TrujiDev <eetgrisales@gmail.com>
2023-12-15 10:48:29 -0500

    Add routing and login functionality to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,17 @@
-function App() {}
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AuthLayout from './layout/AuthLayout';
+import Login from './pages/Login';
+
+function App() {
+	return (
+		<BrowserRouter>
+            <Routes>
+                <Route path="/" element={<AuthLayout />}>
+                    <Route index element={<Login />} />
+                </Route>
+			</Routes>
+		</BrowserRouter>
+	);
+}
 
 export default App;
