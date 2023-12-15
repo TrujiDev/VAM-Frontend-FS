c239bca50a22653697c4f8d4c673665642e4a073
TrujiDev <eetgrisales@gmail.com>
2023-12-15 11:20:52 -0500

    Add routes for registration, account confirmation, and password recovery

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AuthLayout from './layout/AuthLayout';
 import Login from './pages/Login';
+import ConfirmAccount from './pages/ConfirmAccount';
+import ForgotPassword from './pages/ForgotPassword ';
+import Register from './pages/Register';
 
 function App() {
 	return (
 		<BrowserRouter>
-            <Routes>
-                <Route path="/" element={<AuthLayout />}>
-                    <Route index element={<Login />} />
-                </Route>
+			<Routes>
+				<Route
+					path='/'
+					element={<AuthLayout />}
+				>
+					<Route
+						index
+						element={<Login />}
+					/>
+					<Route
+						path='registrarse'
+						element={<Register />}
+					/>
+					<Route
+						path='confirmar/:id'
+						element={<ConfirmAccount />}
+					/>
+					<Route
+						path='recuperar-contrasenia'
+						element={<ForgotPassword />}
+					/>
+				</Route>
 			</Routes>
 		</BrowserRouter>
 	);
