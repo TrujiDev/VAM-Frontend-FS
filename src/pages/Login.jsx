aeab8e7241ff28fae2f3b9f7e9fbbc3f4ba9e596
TrujiDev <eetgrisales@gmail.com>
2023-12-26 23:45:30 -0500

    Update Login page UI

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,15 @@
 const Login = () => {
 	return (
 		<>
-			<h1>Login</h1>
+			<div>
+				<h1 className='text-indigo-600 font-black text-6xl'>
+					Inicia Sesion y Administra tus{' '}
+					<span className='text-black'>Pacientes</span>
+				</h1>
+			</div>
+			<div>
+				<h1>2</h1>
+			</div>
 		</>
 	);
 };
